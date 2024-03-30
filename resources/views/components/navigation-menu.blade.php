ea20b15306b8a4868eb70188c205c1aea57959f3
Azzarip <paride21azzari@gmail.com>
2024-03-30 22:41:27 +0100

    added hamburger menu

--- resources/views/components/navigation-menu.blade.php
+++ resources/views/components/navigation-menu.blade.php
@@ -98,6 +98,11 @@ $items = AdminPanel::items();
             <x-admin-panel::responsive-nav-link href="{{ route('admin.dashboard') }}" :active="request()->routeIs('admin.dashboard')">
                 {{ __('Dashboard') }}
             </x-admin-panel::responsive-nav-link>
+            @foreach ($items as $route => $label)
+            <x-admin-panel::responsive-nav-link href="{{ route('admin', ['panel' => $route]) }}" :active="request()->path() == $route">
+                {{ $label }}
+            </x-admin-panel::responsive-nav-link>
+        @endforeach
         </div>
 
         <!-- Responsive Settings Options -->
